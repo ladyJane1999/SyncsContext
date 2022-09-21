b4dd5d331d6edea31379e1dedf024bd7e6b30469
ladyJane1999 <anzhela.romanova.99@bk.ru>
2022-09-21 09:20:50 +0300

    Добавьте файлы проекта.

---
+++ SyncsContext/Program.cs
@@ -0,0 +1,19 @@
+﻿using SyncsContext;
+
+class Program
+{
+  
+       async static Task Main(String[] args)
+        {
+            CancellationToken token = new CancellationToken();  
+            var cesp = new SingleThreadSynchronizationContext(token);
+             cesp.Post(TestAsyncMethod, null);
+        }
+    
+        async static void TestAsyncMethod(object obj)
+        {
+        Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        await Task.Run(() => Console.WriteLine(Thread.CurrentThread.ManagedThreadId));
+        Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        }
+}
---
+++ SyncsContext/SingleThreadSynchronizationContext.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Concurrent;
+
+namespace SyncsContext
+{
+    sealed class SingleThreadSynchronizationContext :
+             SynchronizationContext
+    {
+        readonly CancellationToken _cancellationToken;
+        readonly BlockingCollection<Tuple<SendOrPostCallback, object>> _queue;
+        bool _completed;
+        private readonly Thread _thread;
+        public SingleThreadSynchronizationContext(CancellationToken cancellationToken)
+        {
+            _cancellationToken = cancellationToken;
+            _queue = new BlockingCollection<Tuple<SendOrPostCallback, object>>();
+            _thread = new Thread(RunOnCurrentThread);
+            _thread.Start(this);
+        }
+        public override void Post(SendOrPostCallback callback, object state)
+        {
+            if (callback == null)
+                throw new ArgumentNullException(nameof(callback));
+            if (_completed)
+                throw new TaskSchedulerException("The synchronization context was already completed");
+            try
+            {
+                _queue.Add(Tuple.Create(callback, state), _cancellationToken);
+            }
+            catch (InvalidOperationException)
+            {
+            }
+        }
+        public override void Send(SendOrPostCallback d, object state)
+        {
+            throw new NotSupportedException("Synchronously sending is not supported.");
+        }
+        public void RunOnCurrentThread(object obj)
+        {
+            SetSynchronizationContext(obj as SynchronizationContext);
+            CancellationToken cancellationToken = new CancellationToken();
+            Tuple<SendOrPostCallback, object> callback;
+            while (_queue.TryTake(out callback, 50, cancellationToken))
+                callback.Item1(callback.Item2);
+        }
+
+        public void SetComplete()
+        {
+            _completed = true;
+        }
+    }
+}
+
